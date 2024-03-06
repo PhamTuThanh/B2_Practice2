57c70e71c87ee31f69d9a389d4a57ca5365d0dc9
thanh <phamtuthanh0410@gmail.com>
2024-03-06 09:59:15 +0700

    update

--- app/src/main/java/com/example/b2_practice2/MainActivity.kt
+++ app/src/main/java/com/example/b2_practice2/MainActivity.kt
@@ -2,10 +2,29 @@ package com.example.b2_practice2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.content.DialogInterface
+import androidx.appcompat.app.AlertDialog
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+    fun exit(view: View){
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Xác nhận")
+        builder.setMessage("Bạn có muốn thoát không?")
+
+        builder.setPositiveButton("Có") { dialog, which ->
+            finish()
+        }
+
+        builder.setNegativeButton("Không") { dialog, which ->
+            dialog.dismiss()
+        }
+
+        val dialog: AlertDialog = builder.create()
+        dialog.show()
     }
 }
